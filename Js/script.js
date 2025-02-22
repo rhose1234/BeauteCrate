81154ec499b7c10e53aa7906f4bb4b747e5e722c
rhose1234 <initial push>
2025-02-22 13:34:31 +0100

    all changes done on landing page

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,3 @@
-document.getElementById('filterButton').addEventListener('click', function() {
-  console.log('Filter button clicked!'); // Debugging line
-  const linkProduct = document.getElementById('linkProduct');
-  linkProduct.classList.toggle('active');
-});
-
-//navigation menu toggle
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.getElementById("menu-toggle");
   const mobileMenu = document.getElementById("mobile-menu");
@@ -15,3 +8,19 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileMenu.classList.toggle("show");
   });
 });
+
+//product page js
+document.getElementById('filterButton').addEventListener('click', function() {
+  console.log('Filter button clicked!'); // Debugging line
+  const linkProduct = document.getElementById('linkProduct');
+  linkProduct.classList.toggle('active');
+});
+
+//filter
+
+document.getElementById("filterbin").addEventListener("click", 
+  function () {
+  let filterContent = document.getElementById("filter-content");
+
+ filterContent.style.display = (filterContent.style.display === "block") ? "none" : "block";
+ });
